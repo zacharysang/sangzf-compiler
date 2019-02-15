ec4990f951d5bb24d56be007a70ac6c061d5948b
Zachary Sang <zacharysang@gmail.com>
2019-02-15 17:33:36 +0000

    main.rs reads program from file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::env;
+use std::fs::File;
+use std::io::*;
+
 // expose token utilities (State struct, Token trait)
 mod tokenize;
 
@@ -12,41 +16,23 @@ fn main() {
   let mut lexer = lexer::Lexer {};
 
   let mut counter = 0;
+
+  let args : Vec<String> = env::args().collect();
+  
+  // take a slice of the input args vector referring to the input program
+  let program_name = &args[1];
   
   // test program
+  let mut program_file = File::open(program_name).expect("Could not open file");
+  let mut program = String::new();
+  
+  program_file.read_to_string(&mut program).expect("Could not read file");
   
   // should have 51 tokens
   //let program = String::from(" program procedure global   ??? variable begin end is type integer float string bool enum if then else for return not true false . ; ( ) , { } - & + < > <= >= == != * / [ ] | := abcdef 1234 898.99 \"stringgoeshere\" \"fancie$t  string_g0es\n\nhere\t\"");
   
   // should have 3 tokens
   //let program = String::from("abc/**this is /*///*a***/*/ doc*/+bcd");
-  
-  // should have 44 tokens
-  let program = String::from("Program MyTestProgram is
-
-integer i;
-char c;
-integer myArray[7:15];
-
-begin
-
-i := 100;
-c := \"A\";
-
-if(i > 100) then
-//do something#
-	i:= 1110;
-end if;
-
-/*Single #Line Block Comments*/
-
-/*
-/*
-Multiline Block Comments*/
-*/
-
-end program.
-");
   
   let mut program_chars = program.chars().peekable();
 
