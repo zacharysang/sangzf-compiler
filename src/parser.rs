3b9cfe9b1de669521417f73efab9bfdba663d7e7
Zachary Sang <zacharysang@gmail.com>
2019-02-20 02:18:08 +0000

    Add program_body parse function

--- src/parser.rs
+++ src/parser.rs
@@ -24,7 +24,7 @@ impl <'a>Parser<'a> {
       if self.program_body() {
         // Check for terminating period
         if let Some(tok_entry) = self.lexer.peek() {
-          if let Token::Period(_tok) = &tok_entry.tok_type {
+          if let Token::Period(_) = &tok_entry.tok_type {
             // consume
             self.lexer.next();
             
@@ -35,14 +35,14 @@ impl <'a>Parser<'a> {
               return true;
             } else if let Some(tok_entry) = self.lexer.peek() {
               // unexpected token after end of program
-              println!("Unexpected token '{}' after end of program", &tok_entry.chars);
+              Self::err_unexpected_tok("<end of program>", &tok_entry.chars);
             }
             
           } else {
-            println!("Unexpected token - Expected '.' got '{}'", &tok_entry.chars);
+            Self::err_unexpected_tok(".", &tok_entry.chars);
           }
         } else {
-          println!("Missing '.' after program body");
+          Self::err_unexpected_end();
         }
       }
     }
@@ -55,47 +55,166 @@ impl <'a>Parser<'a> {
     
     // tok_entry is a borrowed value so it will not be able to be moved
     if let Some(tok_entry) = self.lexer.peek() {
-      if let Token::ProgramKW(_tok) = &tok_entry.tok_type {
+      if let Token::ProgramKW(_) = &tok_entry.tok_type {
         self.lexer.next();
         
         if let Some(tok_entry) = self.lexer.peek() {
-          if let Token::Identifier(_tok) = &tok_entry.tok_type {
+          if let Token::Identifier(_) = &tok_entry.tok_type {
             self.lexer.next();
             
             if let Some(tok_entry) = self.lexer.peek() {
-              if let Token::IsKW(_tok) = &tok_entry.tok_type {
+              if let Token::IsKW(_) = &tok_entry.tok_type {
                 self.lexer.next();
                 return true;
               } else {
-                println!("Unexpected token - Expected 'is', got '{}'", &tok_entry.chars);
+                Self::err_unexpected_tok("is", &tok_entry.chars);
               }
             } else {
-              println!("Unexpected end of program");
+              Self::err_unexpected_end();
             }
             
           } else {
-            println!("Unexpected token - Expected '<identifier>', got '{}'", &tok_entry.chars);
+            Self::err_unexpected_tok("<identifier>", &tok_entry.chars);
           }
         } else {
-          println!("Unexpected end of program");
+          Self::err_unexpected_end();
         }
         
       } else {
-        println!("Unexpected token - Expected 'program' got '{}'", &tok_entry.chars);
+        Self::err_unexpected_tok("program", &tok_entry.chars);
       }
     } else {
-      println!("Unexpected end of program");
+      Self::err_unexpected_end();
     }
   
-    return true;
+    return false;
   }
   
   pub fn program_body(&mut self) -> bool {
   
+    // while next token is in First(declaration), read in a declaration w/ semicolon terminator 
+    loop {
+      if let Some(tok_entry) = self.lexer.peek() {
+
+        // lookahead to see if we should parse a declaration
+        // ie: if next token is in First(declaration)
+        let mut is_declaration = false;
+        match &tok_entry.tok_type {
+          Token::GlobalKW(_) | Token::ProcedureKW(_) | Token::VariableKW(_) | Token::TypeKW(_) => { is_declaration = true; },
+          _ => break
+        }
+        
+        
+        if is_declaration {
+          // parse a declaration (declarations are optional here so no need to check success status)
+          self.declaration();
+          
+          self.semicolon();
+          
+        } else {
+          break;
+        }
+        
+      } else {
+        Self::err_unexpected_end();
+        break;
+      }
+    }
     
+    if let Some(tok_entry) = self.lexer.peek() {
+      if let Token::BeginKW(_) = &tok_entry.tok_type {
+        self.lexer.next();
+        
+        // while next token is in First(statement), read in statement w/ semicolon terminator
+        loop {
+          if let Some(tok_entry) = self.lexer.peek() {
+            
+            let mut is_statement = false;
+            match &tok_entry.tok_type {
+              Token::Identifier(_) | Token::IfKW(_) | Token::ForKW(_) | Token::ReturnKW(_) => { is_statement = true; },
+              _ => break
+            }
+            
+            if is_statement {
+              self.statement();
+              
+              self.semicolon();
+              
+            } else {
+              break;
+            }
+            
+          } else {
+            Self::err_unexpected_end();
+            break;
+          }
+        }
+        
+        // parse the end kw
+        if let Some(tok_entry) = self.lexer.peek() {
+          if let Token::EndKW(_) = &tok_entry.tok_type {
+            self.lexer.next();
+            
+            if let Some(tok_entry) = self.lexer.peek() {
+              if let Token::ProgramKW(_) = &tok_entry.tok_type {
+                self.lexer.next();
+                
+                return true;
+              } else {
+                Self::err_unexpected_tok("program", &tok_entry.chars);
+              }
+            } else {
+              Self::err_unexpected_end();
+            }
+            
+          } else {
+            Self::err_unexpected_tok("end", &tok_entry.chars);
+          }
+        } else {
+          Self::err_unexpected_end();
+        }
+        
+      } else {
+        Self::err_unexpected_tok("begin", &tok_entry.chars);
+      }
+    } else {
+      Self::err_unexpected_end();
+    }
   
+    return false;
+  }
+  
+  pub fn declaration(&mut self) -> bool {
     return true;
   }
   
+  pub fn statement(&mut self) -> bool {
+    return true;
+  }
+  
+  pub fn semicolon(&mut self) -> bool {
+    if let Some(tok_entry) = self.lexer.peek() {
+      if let Token::Semicolon(_) = &tok_entry.tok_type {
+        self.lexer.next();
+        
+        return true;
+      } else {
+        Self::err_unexpected_tok(";", &tok_entry.chars);
+      }
+    } else {
+      Self::err_unexpected_end();
+    }
+    
+    return false;
+  }
+  
+  pub fn err_unexpected_tok(expected: &str, actual: &str) {
+    println!("Unexpected token - Expected: '{}', got: '{}'", expected, actual);
+  }
+  
+  pub fn err_unexpected_end() {
+    println!("Unexpected end of program.");
+  }
   
 }
+
